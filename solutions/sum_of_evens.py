c832d783e8057befbed1821c02edb912cc77c4fd
Thandar Htwe <thandarhtwe2002@gmail.com>
2025-01-09 23:16:50 +0630

    Update sum_of_evens.py

--- solutions/sum_of_evens.py
+++ solutions/sum_of_evens.py
@@ -3,10 +3,8 @@
 """
 This module contains a function to calculate the sum of all even numbers
 from 1 to a given number.
-
 Function:
-- sum_of_evens: Returns the sum of all even numbers within the given range.
-
+  - sum_of_evens: Returns the sum of all even numbers within the given range.
 @author : Thandar Htwer (Marshar)
 Created on Jan 8, 2025
 """
@@ -15,17 +13,13 @@ Created on Jan 8, 2025
 def sum_of_evens(n: int) -> int:
     """
     Calculates the sum of all even numbers from 1 to n.
-
     Parameters:
         n (int): The upper limit of the range (inclusive).
-
     Returns:
         int: The sum of all even numbers from 1 to n.
-
     Raises:
         AssertionError: If the input is not an integer.
         ValueError: If the input is negative.
-
     Examples:
         >>> sum_of_evens(10)
         30
